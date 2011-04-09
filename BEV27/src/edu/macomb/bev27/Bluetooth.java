38aa63608e0e13de02815b54555bfd4ac4c2a5a9
Stephen Cavaliere <stephen@scavaliere.com>
2011-04-09 13:47:33 -0400

    Initial Android Project Commit

---
+++ BEV27/src/edu/macomb/bev27/Bluetooth.java
@@ -0,0 +1,13 @@
+package edu.macomb.bev27;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Bluetooth extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
